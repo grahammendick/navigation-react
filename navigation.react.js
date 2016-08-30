e7cbcdc0707acdfecb178b5a875b8a5e4884d0fa
Graham Mendick <graham.mendick@gmail.com>
2016-08-30 16:10:43 +0100

    Updated to version 2.0.3

--- navigation.react.js
+++ navigation.react.js
@@ -1,5 +1,5 @@
 /**
- * Navigation React v2.0.2
+ * Navigation React v2.0.3
  * (c) Graham Mendick - http://grahammendick.github.io/navigation/
  * License: Apache-2.0
  */
@@ -23,24 +23,35 @@ var LinkUtility = (function () {
             navigationData = stateNavigator.stateContext.includeCurrentData(navigationData);
         return navigationData;
     };
-    LinkUtility.isActive = function (stateNavigator, key, val) {
-        if (!stateNavigator.stateContext.state)
-            return false;
-        if (val != null) {
-            var trackTypes = stateNavigator.stateContext.state.trackTypes;
-            var currentVal = stateNavigator.stateContext.data[key];
-            if (currentVal != null)
-                return trackTypes ? val === currentVal : val.toString() == currentVal.toString();
-            else
-                return val === '';
+    LinkUtility.setActive = function (stateNavigator, props, toProps) {
+        if (!props.activeCssClass && !props.disableActive)
+            return;
+        var active = !!toProps.href;
+        for (var key in props.navigationData) {
+            var val = props.navigationData[key];
+            active = active && (val == null || this.areEqual(val, stateNavigator.stateContext.data[key]));
         }
-        return true;
+        if (active && props.activeCssClass)
+            toProps.className = !toProps.className ? props.activeCssClass : toProps.className + ' ' + props.activeCssClass;
+        if (active && props.disableActive)
+            toProps.href = null;
     };
-    LinkUtility.setActive = function (props, active, activeCssClass, disableActive) {
-        if (active && activeCssClass)
-            props.className = !props.className ? activeCssClass : props.className + ' ' + activeCssClass;
-        if (active && disableActive)
-            props.href = null;
+    LinkUtility.areEqual = function (val, currentVal) {
+        if (currentVal == null)
+            return val == null || val === '';
+        var valType = Object.prototype.toString.call(val);
+        if (valType !== Object.prototype.toString.call(currentVal))
+            return false;
+        if (valType === '[object Array]') {
+            var active = val.length === currentVal.length;
+            for (var i = 0; active && i < val.length; i++) {
+                active = this.areEqual(val[i], currentVal[i]);
+            }
+            return active;
+        }
+        else {
+            return isNaN(val) ? val === currentVal : +val === +currentVal;
+        }
     };
     LinkUtility.isValidAttribute = function (attr) {
         return attr !== 'stateNavigator' && attr !== 'stateKey' && attr !== 'navigationData' && attr !== 'includeCurrentData'
@@ -96,10 +107,15 @@ var LinkUtility = _dereq_('./LinkUtility');
 var React = (typeof window !== "undefined" ? window['React'] : typeof global !== "undefined" ? global['React'] : null);
 var NavigationBackLink = (function (_super) {
     __extends(NavigationBackLink, _super);
-    function NavigationBackLink() {
+    function NavigationBackLink(props, context) {
         var _this = this;
-        _super.apply(this, arguments);
-        this.onNavigate = function () { return _this.forceUpdate(); };
+        _super.call(this, props, context);
+        this.onNavigate = function () {
+            if (_this.state.stateContext !== _this.getStateNavigator().stateContext.url
+                || _this.state.crumb !== _this.getNavigationBackLink())
+                _this.setState(_this.getNextState());
+        };
+        this.state = this.getNextState();
     }
     NavigationBackLink.prototype.getStateNavigator = function () {
         return this.props.stateNavigator || this.context.stateNavigator;
@@ -108,9 +124,18 @@ var NavigationBackLink = (function (_super) {
         var _this = this;
         return LinkUtility.getLink(this.getStateNavigator(), function () { return _this.getStateNavigator().getNavigationBackLink(_this.props.distance); });
     };
+    NavigationBackLink.prototype.getNextState = function () {
+        return {
+            stateContext: this.getStateNavigator().stateContext.url,
+            crumb: this.getNavigationBackLink()
+        };
+    };
     NavigationBackLink.prototype.componentDidMount = function () {
         if (!this.props.lazy)
             this.getStateNavigator().onNavigate(this.onNavigate);
+    };
+    NavigationBackLink.prototype.componentWillReceiveProps = function () {
+        this.setState(this.getNextState());
     };
     NavigationBackLink.prototype.componentWillUnmount = function () {
         if (!this.props.lazy)
@@ -147,10 +172,14 @@ var LinkUtility = _dereq_('./LinkUtility');
 var React = (typeof window !== "undefined" ? window['React'] : typeof global !== "undefined" ? global['React'] : null);
 var NavigationLink = (function (_super) {
     __extends(NavigationLink, _super);
-    function NavigationLink() {
+    function NavigationLink(props, context) {
         var _this = this;
-        _super.apply(this, arguments);
-        this.onNavigate = function () { return _this.forceUpdate(); };
+        _super.call(this, props, context);
+        this.onNavigate = function () {
+            if (_this.state.stateContext !== _this.getStateNavigator().stateContext.url)
+                _this.setState(_this.getNextState());
+        };
+        this.state = this.getNextState();
     }
     NavigationLink.prototype.getStateNavigator = function () {
         return this.props.stateNavigator || this.context.stateNavigator;
@@ -160,9 +189,15 @@ var NavigationLink = (function (_super) {
         var navigationData = LinkUtility.getData(this.getStateNavigator(), this.props.navigationData, this.props.includeCurrentData, this.props.currentDataKeys);
         return LinkUtility.getLink(this.getStateNavigator(), function () { return _this.getStateNavigator().getNavigationLink(_this.props.stateKey, navigationData); });
     };
+    NavigationLink.prototype.getNextState = function () {
+        return { stateContext: this.getStateNavigator().stateContext.url };
+    };
     NavigationLink.prototype.componentDidMount = function () {
         if (!this.props.lazy)
             this.getStateNavigator().onNavigate(this.onNavigate);
+    };
+    NavigationLink.prototype.componentWillReceiveProps = function () {
+        this.setState(this.getNextState());
     };
     NavigationLink.prototype.componentWillUnmount = function () {
         if (!this.props.lazy)
@@ -175,14 +210,10 @@ var NavigationLink = (function (_super) {
             if (LinkUtility.isValidAttribute(key))
                 props[key] = this.props[key];
         }
-        var active = true;
-        for (var key in this.props.navigationData) {
-            active = active && LinkUtility.isActive(this.getStateNavigator(), key, this.props.navigationData[key]);
-        }
         props.href = this.getNavigationLink();
         LinkUtility.addListeners(this, this.getStateNavigator(), this.props, props, function () { return _this.getNavigationLink(); });
-        active = active && !!props.href && this.getStateNavigator().stateContext.state && this.getStateNavigator().stateContext.state.key === this.props.stateKey;
-        LinkUtility.setActive(props, active, this.props.activeCssClass, this.props.disableActive);
+        if (this.getStateNavigator().stateContext.state && this.getStateNavigator().stateContext.state.key === this.props.stateKey)
+            LinkUtility.setActive(this.getStateNavigator(), this.props, props);
         return React.createElement('a', props, this.props.children);
     };
     NavigationLink.contextTypes = {
@@ -219,10 +250,14 @@ var LinkUtility = _dereq_('./LinkUtility');
 var React = (typeof window !== "undefined" ? window['React'] : typeof global !== "undefined" ? global['React'] : null);
 var RefreshLink = (function (_super) {
     __extends(RefreshLink, _super);
-    function RefreshLink() {
+    function RefreshLink(props, context) {
         var _this = this;
-        _super.apply(this, arguments);
-        this.onNavigate = function () { return _this.forceUpdate(); };
+        _super.call(this, props, context);
+        this.onNavigate = function () {
+            if (_this.state.stateContext !== _this.getStateNavigator().stateContext.url)
+                _this.setState(_this.getNextState());
+        };
+        this.state = this.getNextState();
     }
     RefreshLink.prototype.getStateNavigator = function () {
         return this.props.stateNavigator || this.context.stateNavigator;
@@ -232,9 +267,15 @@ var RefreshLink = (function (_super) {
         var navigationData = LinkUtility.getData(this.getStateNavigator(), this.props.navigationData, this.props.includeCurrentData, this.props.currentDataKeys);
         return LinkUtility.getLink(this.getStateNavigator(), function () { return _this.getStateNavigator().getRefreshLink(navigationData); });
     };
+    RefreshLink.prototype.getNextState = function () {
+        return { stateContext: this.getStateNavigator().stateContext.url };
+    };
     RefreshLink.prototype.componentDidMount = function () {
         if (!this.props.lazy)
             this.getStateNavigator().onNavigate(this.onNavigate);
+    };
+    RefreshLink.prototype.componentWillReceiveProps = function () {
+        this.setState(this.getNextState());
     };
     RefreshLink.prototype.componentWillUnmount = function () {
         if (!this.props.lazy)
@@ -247,14 +288,9 @@ var RefreshLink = (function (_super) {
             if (LinkUtility.isValidAttribute(key))
                 props[key] = this.props[key];
         }
-        var active = true;
-        for (var key in this.props.navigationData) {
-            active = active && LinkUtility.isActive(this.getStateNavigator(), key, this.props.navigationData[key]);
-        }
         props.href = this.getRefreshLink();
         LinkUtility.addListeners(this, this.getStateNavigator(), this.props, props, function () { return _this.getRefreshLink(); });
-        active = active && !!props.href;
-        LinkUtility.setActive(props, active, this.props.activeCssClass, this.props.disableActive);
+        LinkUtility.setActive(this.getStateNavigator(), this.props, props);
         return React.createElement('a', props, this.props.children);
     };
     RefreshLink.contextTypes = {
